a46d4498b49a341ec6d6d90c0cdb41855d8d9957
gzy <283211498@qq.com>
2020-02-11 19:02:32 +0800

    add 登录 cookie session

--- 动态服务器/server.js
+++ 动态服务器/server.js
@@ -22,9 +22,60 @@ var server = http.createServer(function (request, response) {
   /******** 从这里开始看，上面不要看 ************/
 
   console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
+  const session = JSON.parse(fs.readFileSync('./session.json').toString())
 
-
-  if (path === "/register" && method === "POST") {
+  if (path === "/sign_in" && method === "POST") {
+    response.setHeader('Content-type', 'text/html;charset=utf-8')
+    const usersArray = JSON.parse(fs.readFileSync('./db/users.json'))
+    const array = []
+    request.on('data', chunk => {
+      array.push(chunk)
+    })
+    request.on('end', () => {
+      const string = Buffer.concat(array).toString()
+      const obj = JSON.parse(string)
+      const user = usersArray.find(user =>
+          user.name === obj.name && user.password === obj.password
+          // 返回第一个匹配的 or undefined
+      )
+      if (user === undefined) {
+        response.statusCode = 400
+        response.end(`{"errorCode:" 4001}`)
+      } else {
+        response.statusCode = 200
+        const random = Math.random()
+        session[random] = {user_id: user.id}
+        console.log(session)
+        console.log(1)
+        fs.writeFileSync('./session.json', JSON.stringify(session))
+        response.setHeader('Set-Cookie', `session_id=${random}; HttpOnly`)
+        response.end()
+      }
+    })
+  } else if (path === '/home.html') {
+    const cookie = request.headers['cookie'] //没有cookie是undefined
+    // console.log(cookie)
+    let sessionId
+    try {
+      sessionId = cookie.split(";").filter(s => s.indexOf("session_id=") !== -1)[0].split('=')[1]
+    } catch (error) {
+    }
+    // console.log(sessionId)
+    let string = ''
+    const homeHtml = fs.readFileSync('./public/home.html').toString()
+    if (sessionId && session[sessionId]) {
+      const userId = session[sessionId].user_id
+      const usersArray = JSON.parse(fs.readFileSync('./db/users.json'))
+      const user = usersArray.find(user => user.id === userId)
+      if (user) {
+        string = homeHtml.replace('{{loginStatus}}', '已登录').replace('{{userName}}', user.name)
+      }
+    } else {
+      string = homeHtml.replace('{{loginStatus}}', '未登录').replace('{{userName}}', '')
+    }
+    response.write(string)
+    response.end()
+  } else if (path === "/register" && method === "POST") {
     response.setHeader('Content-type', 'text/html;charset=utf-8')
     const usersArray = JSON.parse(fs.readFileSync('./db/users.json'))  // 数据库里的数据
     const array = []
